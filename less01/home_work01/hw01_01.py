dad1162f67dd371f53c8b66a0d55fed1f568cffc
Viktor Filinskij <v.filinskij@gmail.com>
2020-09-28 22:13:42 +0300

    change constant name to upper-case, use for with  isintance to detect var type

--- less01/home_work01/hw01_01.py
+++ less01/home_work01/hw01_01.py
@@ -6,21 +6,21 @@ __author__ = 'Viktor Filinskij'
 # Затем с помощью онлайн-конвертера преобразовать строковые представление
 # в формат Unicode и также проверить тип и содержимое переменных.
 
-word1_str = 'разработка'
-word2_str = 'сокет'
-word3_str = 'декоратор'
-word1_utf8 = b'\xd1\x80\xd0\xb0\xd0\xb7\xd1\x80\xd0\xb0\xd0\xb1\xd0\xbe\xd1\x82\xd0\xba\xd0\xb0'
-word2_utf8 = b'\xd1\x81\xd0\xbe\xd0\xba\xd0\xb5\xd1\x82'
-word3_utf8 = b'\xd0\xb4\xd0\xb5\xd0\xba\xd0\xbe\xd1\x80\xd0\xb0\xd1\x82\xd0\xbe\xd1\x80'
+WORD1_STR = 'разработка'
+WORD2_STR = 'сокет'
+WORD3_STR = 'декоратор'
+WORD1_UTF8 = b'\xd1\x80\xd0\xb0\xd0\xb7\xd1\x80\xd0\xb0\xd0\xb1\xd0\xbe\xd1\x82\xd0\xba\xd0\xb0'
+WORD2_UTF8 = b'\xd1\x81\xd0\xbe\xd0\xba\xd0\xb5\xd1\x82'
+WORD3_UTF8 = b'\xd0\xb4\xd0\xb5\xd0\xba\xd0\xbe\xd1\x80\xd0\xb0\xd1\x82\xd0\xbe\xd1\x80'
 
 
 def main():
-    print(f"String \"{word1_str}\", type: {type(word1_str)}")
-    print(f"String \"{word2_str}\", type: {type(word2_str)}")
-    print(f"String \"{word3_str}\", type: {type(word3_str)}")
-    print(f"Byte sequence \"{word1_utf8.decode('utf-8', 'strict')}\", type: {type(word1_utf8)}")
-    print(f"Byte sequence \"{word2_utf8.decode('utf-8', 'strict')}\", type: {type(word2_utf8)}")
-    print(f"Byte sequence \"{word3_utf8.decode('utf-8', 'strict')}\", type: {type(word3_utf8)}")
+
+    for var in [WORD1_STR, WORD2_STR, WORD3_STR, WORD1_UTF8, WORD2_UTF8, WORD3_UTF8]:
+        if isinstance(var, str):
+            print(f"String \"{var}\", type: {type(var)}")
+        elif isinstance(var, bytes):
+            print(f"Byte sequence \"{var.decode('utf-8', 'strict')}\", type: {type(var)}")
 
 
 if __name__ == '__main__':
