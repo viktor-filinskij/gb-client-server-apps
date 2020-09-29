41846fc7c7b63096ea079f674249cd9d119c1aaa
Viktor Filinskij <v.filinskij@gmail.com>
2020-09-29 20:42:53 +0300

    use subprocess to execute ping, return output as text in utf-8

--- less01/home_work01/hw01_05.py
+++ less01/home_work01/hw01_05.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 __author__ = 'Viktor Filinskij'
 
+# Выполнить пинг веб-ресурсов yandex.ru, youtube.com и преобразовать
+# результаты из байтовового в строковый тип на кириллице.
+
 import subprocess
 
 
-exe = 'ping'
-dst = ['yandex.ru', 'youtube.com']
-
+COMMAND = 'ping'
+COMMAND_PARAMS = '-c 4'
+destination = ['yandex.ru', 'youtube.com']
 
 
 def main():
-    pass
+    for target in destination:
+        completed_process = subprocess.run([COMMAND, COMMAND_PARAMS, target],
+                                           check=True,
+                                           encoding='utf-8',
+                                           capture_output=True).stdout
+        print(completed_process)
 
 
 if __name__ == '__main__':
