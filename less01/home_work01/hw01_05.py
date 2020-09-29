379a727e3b5bd9d37adb14db3c373db11463713f
Viktor Filinskij <viktor.filinskij@oxylabs.io>
2020-09-29 18:34:10 +0300

    Add hw01_05.py

---
+++ less01/home_work01/hw01_05.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+__author__ = 'Viktor Filinskij'
+
+import subprocess
+
+
+exe = 'ping'
+dst = ['yandex.ru', 'youtube.com']
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
