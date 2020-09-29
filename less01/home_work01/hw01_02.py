7987bd51650be9b39f18a6f5854672272424b13f
Viktor Filinskij <v.filinskij@gmail.com>
2020-09-29 19:56:50 +0300

    fix

--- less01/home_work01/hw01_02.py
+++ less01/home_work01/hw01_02.py
@@ -6,9 +6,9 @@ __author__ = 'Viktor Filinskij'
 # (не используя методы encode и decode) и определить тип, содержимое и длину
 # соответствующих переменных.
 
-WORD1_UTF8 = b'\x63\x6c\x61\x73\x73'  # class
-WORD2_UTF8 = b'\x66\x75\x6e\x63\x74\x69\x6f\x6e'  # function
-WORD3_UTF8 = b'\x6d\x65\x74\x68\x6f\x64'  # method
+WORD1_UTF8 = b'class'  # \x63\x6c\x61\x73\x73'  # class
+WORD2_UTF8 = b'function'  # \x66\x75\x6e\x63\x74\x69\x6f\x6e'  # function
+WORD3_UTF8 = b'method'  # \x6d\x65\x74\x68\x6f\x64'  # method
 
 
 def main():
